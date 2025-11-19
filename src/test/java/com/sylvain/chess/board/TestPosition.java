7f1b8a201f470eb754e5b90ade3e942d1a812a97
Syl <sylv1fournier@hotmail.com>
2025-11-19 14:45:36 -0300

    Other tests of board limits

--- src/test/java/com/sylvain/chess/board/TestPosition.java
+++ src/test/java/com/sylvain/chess/board/TestPosition.java
@@ -1,12 +1,17 @@
 package com.sylvain.chess.board;
 
+import com.sylvain.chess.Constants;
 import com.sylvain.chess.pieces.King;
+import com.sylvain.chess.pieces.Queen;
+import com.sylvain.chess.pieces.Rook;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestPosition {
     @Test
-    public void testOutOfBoard() {
+    public void testBoardLimits() {
         Assert.assertFalse((new Position(new King(), 0, 5)).isValid());
+        Assert.assertFalse((new Position(new Rook(), 2, Constants.BOARD_ROWS + 1)).isValid());
+        Assert.assertTrue((new Position(new Queen(), 1, 1)).isValid());
     }
 }
