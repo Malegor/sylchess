9b783388b436286a08b1fd8a20165f7aef20c559
Syl <sylv1fournier@hotmail.com>
2025-11-19 23:01:14 -0300

    TestKnight

---
+++ src/test/java/com/sylvain/chess/pieces/TestKnight.java
@@ -0,0 +1,27 @@
+package com.sylvain.chess.pieces;
+
+import com.sylvain.chess.Constants;
+import com.sylvain.chess.board.ChessBoard;
+import com.sylvain.chess.board.Square;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Set;
+
+public class TestKnight {
+    @Test
+    public void testControlledSquares() {
+        for (int i = 1; i <= Constants.BOARD_COLS; i++) {
+            for (int j = 1; j <= Constants.BOARD_ROWS; j++) {
+                final Square startingSquare = new Square(i, j);
+                final Set<Square> squares = Piece.KNIGHT.getControlledSquares(startingSquare);
+                System.out.println(startingSquare + " : " + squares);
+                Assert.assertTrue(squares.size() >= 2 && squares.size() <= 8);
+                for (Square square : squares) {
+                    Assert.assertTrue(ChessBoard.isInBoard(square));
+                    Assert.assertEquals(3, Math.abs(square.getColumn() - i) + Math.abs(square.getRow() - j));
+                }
+            }
+        }
+    }
+}
--- src/test/java/com/sylvain/chess/pieces/TestPawn.java
+++ src/test/java/com/sylvain/chess/pieces/TestPawn.java
@@ -15,13 +15,13 @@ public class TestPawn {
         for (int i = 1; i <= Constants.BOARD_COLS; i++) {
             final Square startingSquare = new Square(i, j);
             final Set<Square> squares = Piece.PAWN.getControlledSquares(startingSquare);
+            System.out.println(startingSquare + " : " + squares);
             Assert.assertEquals((i == 1 || i == Constants.BOARD_COLS) ? 1 : 2, squares.size());
             for (Square square : squares) {
                 Assert.assertTrue(ChessBoard.isInBoard(square));
                 Assert.assertEquals(1, square.getRow() - startingSquare.getRow());
                 Assert.assertEquals(1, Math.abs(square.getColumn() - startingSquare.getColumn()));
             }
-            System.out.println(startingSquare + " : " + squares);
         }
     }
 }
