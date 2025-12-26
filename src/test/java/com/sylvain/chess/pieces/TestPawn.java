64387e5923a5addce4dac212f1a1ffb58b4ddf70
Syl <sylv1fournier@hotmail.com>
2025-12-26 19:22:12 -0300

    Test that fails, only for blacks

--- src/test/java/com/sylvain/chess/pieces/TestPawn.java
+++ src/test/java/com/sylvain/chess/pieces/TestPawn.java
@@ -3,6 +3,7 @@ package com.sylvain.chess.pieces;
 import com.sylvain.chess.Color;
 import com.sylvain.chess.board.ChessBoard;
 import com.sylvain.chess.board.Square;
+import com.sylvain.chess.moves.Move;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -23,5 +24,35 @@ public class TestPawn {
                 Assert.assertEquals(1, Math.abs(square.getColumn() - startingSquare.getColumn()));
             }
         }
+    }
+
+    @Test
+    public void testPossibleCaptures() {
+        final ChessBoard board = new ChessBoard();
+        board.addPiece(new Pawn(Color.WHITE, new Square(4, 4)));
+        board.addPiece(new Pawn(Color.BLACK, new Square(3, 6)));
+        board.addPiece(new Pawn(Color.BLACK, new Square(4, 6)));
+        board.addPiece(new Pawn(Color.BLACK, new Square(5, 6)));
+        board.addPiece(new Pawn(Color.BLACK, new Square(3, 5)));
+        board.addPiece(new Pawn(Color.BLACK, new Square(4, 5)));
+        board.addPiece(new Pawn(Color.BLACK, new Square(5, 5)));
+        final List<Move> allValidMoves = board.getAllValidMoves(Color.WHITE);
+        System.out.println(allValidMoves);
+        Assert.assertEquals(2, allValidMoves.size());
+    }
+
+    @Test
+    public void testPossibleCapturesBlack() {
+        final ChessBoard board = new ChessBoard();
+        board.addPiece(new Pawn(Color.BLACK, new Square(4, 4)));
+        board.addPiece(new Pawn(Color.WHITE, new Square(3, 2)));
+        board.addPiece(new Pawn(Color.WHITE, new Square(4, 2)));
+        board.addPiece(new Pawn(Color.WHITE, new Square(5, 2)));
+        board.addPiece(new Pawn(Color.WHITE, new Square(3, 3)));
+        board.addPiece(new Pawn(Color.WHITE, new Square(4, 3)));
+        board.addPiece(new Pawn(Color.WHITE, new Square(5, 3)));
+        final List<Move> allValidMoves = board.getAllValidMoves(Color.BLACK);
+        System.out.println(allValidMoves);
+        Assert.assertEquals(2, allValidMoves.size());
     }
 }
