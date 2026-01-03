1b28cdcbf5b83cfa43e06d0a55bbbec6de717713
Syl <sylv1fournier@hotmail.com>
2026-01-03 16:25:25 -0300

    Other tests

--- src/test/java/com/sylvain/chess/io/TestLoadPosition.java
+++ src/test/java/com/sylvain/chess/io/TestLoadPosition.java
@@ -13,4 +13,20 @@ public class TestLoadPosition {
     System.out.println(chessBoard.getPositionString());
     Assert.assertEquals("Ra1;Nb1;Bc1;Qd1;Ke1;Bf1;Ng1;Rh1;Pa2;Pb2;Pc2;Pd2;Pe2;Pf2;Pg2;Ph2;pa7;pb7;pc7;pd7;pe7;pf7;pg7;ph7;ra8;nb8;bc8;qd8;ke8;bf8;ng8;rh8;", chessBoard.getPositionString());
   }
+
+  @Test
+  public void testAfterMovingPawn() {
+    final ChessBoard chessBoard = FenDecoder.loadBoard("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR");
+    chessBoard.printBoard();
+    System.out.println(chessBoard.getPositionString());
+    Assert.assertEquals("Ra1;Nb1;Bc1;Qd1;Ke1;Bf1;Ng1;Rh1;Pa2;Pb2;Pc2;Pd2;Pf2;Pg2;Ph2;Pe4;pa7;pb7;pc7;pd7;pe7;pf7;pg7;ph7;ra8;nb8;bc8;qd8;ke8;bf8;ng8;rh8;", chessBoard.getPositionString());
+  }
+
+  @Test
+  public void testMateIn3() {
+    final ChessBoard chessBoard = FenDecoder.loadBoard("1kr4r/ppp2p2/5bpq/4N3/4PP2/1b4P1/PPP2Q1P/R5K1");
+    chessBoard.printBoard();
+    System.out.println(chessBoard.getPositionString());
+    Assert.assertEquals("Ra1;Kg1;Pa2;Pb2;Pc2;Qf2;Ph2;bb3;Pg3;Pe4;Pf4;Ne5;bf6;pg6;qh6;pa7;pb7;pc7;pf7;kb8;rc8;rh8;", chessBoard.getPositionString());
+  }
 }
