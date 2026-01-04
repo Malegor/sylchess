83e3b3b36d4b4f489dace7cfbfc78397b46ca3e6
Syl <sylv1fournier@hotmail.com>
2026-01-04 14:38:29 -0300

    New asserts

--- src/test/java/com/sylvain/chess/play/TestFullDummyGame.java
+++ src/test/java/com/sylvain/chess/play/TestFullDummyGame.java
@@ -2,6 +2,8 @@ package com.sylvain.chess.play;
 
 import com.sylvain.chess.Color;
 import com.sylvain.chess.board.ChessBoard;
+import com.sylvain.chess.pieces.Pawn;
+import com.sylvain.chess.pieces.PieceOnBoard;
 import com.sylvain.chess.play.players.DummyPlayer;
 import org.junit.Assert;
 import org.junit.Test;
@@ -15,7 +17,23 @@ public class TestFullDummyGame {
     final Gameplay play = new Gameplay(board, List.of(new DummyPlayer(Color.WHITE), new DummyPlayer(Color.BLACK)));
     final GameStatus gameStatus = play.playGame();
     System.out.println(gameStatus + " after " + play.getMoveNumber() + " moves.");
-    // OBS: checkmate moving white's king!
+    // OBS: checkmate by moving white's king!
+    Assert.assertEquals(GameStatus.CHECKMATE, gameStatus);
+    Assert.assertEquals(57, play.getMoveNumber());
+    int numberOfUnmovedPawns = 0;
+    for (PieceOnBoard piece : board.getPieces(Color.WHITE).values()) {
+      // OBS: one single pawn didn't move during the game
+      if (piece.getSquare().getRow() != 2 || !(piece instanceof Pawn))
+        Assert.assertTrue(piece.isHasAlreadyMoved());
+      else {
+        numberOfUnmovedPawns++;
+        Assert.assertFalse(piece.isHasAlreadyMoved());
+      }
+    }
+    for (PieceOnBoard piece : board.getPieces(Color.BLACK).values()) {
+      Assert.assertTrue(piece.isHasAlreadyMoved());
+    }
+    Assert.assertEquals(1, numberOfUnmovedPawns);
   }
 
   @Test
