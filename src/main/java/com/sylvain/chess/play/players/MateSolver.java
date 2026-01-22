a20fbb2a015b5db87322175d7426222ddfdee169
Syl <sylv1fournier@hotmail.com>
2026-01-21 23:13:54 -0300

    First alphabeta version

--- src/main/java/com/sylvain/chess/play/players/MateSolver.java
+++ src/main/java/com/sylvain/chess/play/players/MateSolver.java
@@ -12,6 +12,8 @@ import java.util.List;
  * May be easier for a first version DFS up to a maximum number of moves.
  */
 public class MateSolver extends Player {
+  private final static int MAX_VALUE = 500;
+
   private final int maxNumberOfMoves;
 
   public MateSolver(final Color color, final ChessBoard board, final int maxNumberOfMoves) {
@@ -21,6 +23,10 @@ public class MateSolver extends Player {
 
   @Override
   protected Move selectMove(final List<Move> validMoves) {
+    final int value = alphabeta(null, this.maxNumberOfMoves * 2 - 1, - MAX_VALUE, MAX_VALUE);
+    System.out.println(value);
+    return null;
+    /*
     final Color oppositeColor = ChessBoard.getOppositeColor(this.color);
     final Comparator<Move> checkComparator = (m1, m2) -> Boolean.compare(this.board.checksOppositeKing(m2.getDestinationPiece()),
             this.board.checksOppositeKing(m1.getDestinationPiece()));
@@ -57,5 +63,57 @@ public class MateSolver extends Player {
       }
     }
     return null; // resign
+     */
+  }
+
+  private int alphabeta(final Move move, final int depth, int alpha, int beta) { // TODO make alpha & beta NOT parameters(?)
+    if (move != null)
+      move.simulateForCheckValidate();
+    final Comparator<Move> checkComparator = (m1, m2) -> Boolean.compare(this.board.checksOppositeKing(m2.getDestinationPiece()),
+            this.board.checksOppositeKing(m1.getDestinationPiece()));
+    final Color currentColor = move == null ? ChessBoard.getOppositeColor(this.color) : move.getColor();
+    final Color oppositeColor = ChessBoard.getOppositeColor(currentColor);
+    final List<Move> allValidMovesForOpponent = this.board.findAllValidMoves(oppositeColor).stream().sorted(checkComparator).toList(); // TODO: order moves by checking king
+    if (depth == 0 || allValidMovesForOpponent.isEmpty()) {
+      final int evaluation = this.evaluateBoardFor(currentColor, allValidMovesForOpponent);
+      if (move != null)
+        move.rollback();
+      // TODO: return move+evaluation --> new attribute evaluation in Move?
+      // TODO: avoid evaluating same position several times => map (position+color, eval)
+      return evaluation;
+    }
+    boolean shouldMaximize = oppositeColor == this.color;
+    int multiplier = shouldMaximize ? 1 : -1;
+    int bestValue = - multiplier * MAX_VALUE;
+    for (final Move moveOpponent : allValidMovesForOpponent) {
+      bestValue = multiplier * Math.max(multiplier * bestValue, multiplier * alphabeta(moveOpponent, depth - 1, alpha, beta));
+      if (multiplier * bestValue >= (shouldMaximize ? beta : multiplier * alpha)) // alpha or beta cutoff
+        break;
+    }
+    if (shouldMaximize) {
+      alpha = Math.max(alpha, bestValue);
+    }
+    else
+      beta = Math.min(beta, bestValue);
+    if (move != null)
+      move.rollback();
+    return bestValue;
+  }
+
+  /**
+   *
+   * @param color - The color that has just played its move
+   * @param allValidMovesForOpponent - All the possible moves for the opponent
+   * @return The evaluation of the position after the move.
+   */
+  private int evaluateBoardFor(final Color color, final List<Move> allValidMovesForOpponent) {
+    boolean shouldMaximize = color == this.color;
+    int multiplier = shouldMaximize ? 1 : -1;
+    final Color oppositeColor = ChessBoard.getOppositeColor(color);
+    if (this.board.isKingCheckMate(oppositeColor)) {
+      return multiplier * MAX_VALUE;
+    }
+    // TODO: more complete evaluation: count pieces "values" etc.
+    return 0;
   }
 }
